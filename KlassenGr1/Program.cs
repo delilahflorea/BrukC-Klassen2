9bd6df52cac860b9b6ac9872110204c6860cf625
Delilah Florea <delilah_florea@yahoo.com>
2024-11-11 14:27:08 +0200

    Florea2

--- KlassenGr1/Program.cs
+++ KlassenGr1/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Florea Delilah");
+            Console.WriteLine("Florea Delilah2");
         }
     }
 }
